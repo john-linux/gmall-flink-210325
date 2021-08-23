d1c01872b51c3595f956c8907f41dc0dd84cd6aa
harry <harry@atguigu.com>
2021-08-23 10:32:19 +0800

    day03(1)

--- gmall-flink-cdc/src/main/java/com/atguigu/FlinkCDCWithCustomerDeserialization.java
+++ gmall-flink-cdc/src/main/java/com/atguigu/FlinkCDCWithCustomerDeserialization.java
@@ -4,30 +4,18 @@ import com.alibaba.ververica.cdc.connectors.mysql.MySQLSource;
 import com.alibaba.ververica.cdc.connectors.mysql.table.StartupOptions;
 import com.alibaba.ververica.cdc.debezium.DebeziumSourceFunction;
 import com.alibaba.ververica.cdc.debezium.StringDebeziumDeserializationSchema;
-import org.apache.flink.api.common.restartstrategy.RestartStrategies;
-import org.apache.flink.runtime.executiongraph.restart.RestartStrategy;
 import org.apache.flink.runtime.state.filesystem.FsStateBackend;
 import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
-public class FlinkCDC {
+public class FlinkCDCWithCustomerDeserialization {
 
     public static void main(String[] args) throws Exception {
 
         //1.获取执行环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
-
-        //1.1 开启CK并指定状态后端为FS    memory  fs  rocksdb
-        env.setStateBackend(new FsStateBackend("hdfs://hadoop102:8020/gmall-flink-210325/ck"));
-        env.enableCheckpointing(5000L);
-        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
-        env.getCheckpointConfig().setCheckpointTimeout(10000L);
-        env.getCheckpointConfig().setMaxConcurrentCheckpoints(2);
-        env.getCheckpointConfig().setMinPauseBetweenCheckpoints(3000);
-
-        //env.setRestartStrategy(RestartStrategies.fixedDelayRestart());
 
         //2.通过FlinkCDC构建SourceFunction并读取数据
         DebeziumSourceFunction<String> sourceFunction = MySQLSource.<String>builder()
@@ -37,7 +25,7 @@ public class FlinkCDC {
                 .password("000000")
                 .databaseList("gmall-210325-flink")
                 .tableList("gmall-210325-flink.base_trademark")   //如果不添加该参数,则消费指定数据库中所有表的数据.如果指定,指定方式为db.table
-                .deserializer(new StringDebeziumDeserializationSchema())
+                .deserializer(new CustomerDeserialization())
                 .startupOptions(StartupOptions.initial())
                 .build();
         DataStreamSource<String> streamSource = env.addSource(sourceFunction);
@@ -46,7 +34,7 @@ public class FlinkCDC {
         streamSource.print();
 
         //4.启动任务
-        env.execute("FlinkCDC");
+        env.execute("FlinkCDCWithCustomerDeserialization");
 
     }
 
